89ce506dcbad0322dcb66b8ba130e6570da2edff
Elijah Delos Reyes <elijah@Elijahs-MBP.lan>
2021-05-30 14:42:09 -0700

    v2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,43 @@
 import "./AppV2.css";
 // import HeroComponent from "./components/HeroComponent";
 import HeroComponentV2 from "./components/HeroComponentV2";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <div className="App">
-      {/* <HeroComponent /> */}
-      <HeroComponentV2 />
+      <div style={{ height: "50px" }} />
+      <Particles
+        params={{
+          particles: {
+            color: {
+              value: "#00b1d2",
+            },
+            line_linked: {
+              color: {
+                value: "#FDDB27",
+              },
+            },
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 3,
+            },
+          },
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <HeroComponentV2 />
+      </div>
     </div>
   );
 }
